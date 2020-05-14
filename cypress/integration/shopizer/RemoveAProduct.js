98d15f923c18ac3b1d12ea30806a2a1570bab129
nik-gus <niklas.gustafsson@gmail.com>
2020-05-14 13:10:40 +0200

    added RemoveAProduct.js

---
+++ cypress/integration/shopizer/RemoveAProduct.js
@@ -0,0 +1,44 @@
+//As a customer i would like to add products to the cart so that i can remove which i dont want.
+
+describe('As a Customer i would like to purchase a laptopbag', ()=> {
+    it('Removes the product to the cart', () => {
+cy.visit("http://localhost:8080") 
+
+cy      
+        .get('a')
+        .contains(" Laptop Bags")
+        .click();
+
+
+cy      .get('#productsContainer > .col-md-4 > .product-content > .store-btn > .store-btn-addtocart > .addToCart')
+        .click()
+
+cy
+        .get('a[productid=8]')
+        .contains('Add to cart')
+        .click()
+
+cy      .get('.cc-btn')
+        .click()
+
+cy
+        .get('#hiddenSearchForm')
+        .click({ force: true });
+
+cy      
+        .contains('Checkout')
+        .click({ force: true });
+
+cy      .get('h1')
+        .contains('Review your order')
+
+cy      .get('.cart-del')
+        .click()
+/*
+cy      .get('.open > .dropdown-toggle > .hidden-xs')
+        .contains('No items in your shopping cart')
+
+        */
+
+});
+});
